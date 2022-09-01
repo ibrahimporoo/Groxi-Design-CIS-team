b057a06ead8f9ee35d2d3d2f5738d62f61f34221
Ibrahim Abdo <ibrahimporoo@gmail.com>
2022-09-02 00:11:18 +0200

    Upload Some Custom Files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Groxi-Design-CIS-team
-This is 'Groxi-Design'  which I designed to practice my HTML, CSS, JS skills.
--- js/shop.js
+++ js/shop.js
@@ -14,4 +14,38 @@ showFilter.addEventListener("click", _ => {
 			showFilter.innerHTML = "<i class='fa-solid fa-filter' style='text-decoration: line-through'></i> Hide Filter";
 		}
 	})
+});
+// Search Field
+let searchField = document.getElementById("searchInput");
+let namedItems = document.querySelectorAll(".products .box h3");
+searchField.addEventListener("keyup", function(e) {
+	namedItems.forEach(item => {
+		let categoryName = item.textContent.toLowerCase();
+		let userSearchInput = e.target.value.toLowerCase();
+		if(categoryName.indexOf(userSearchInput) !== -1 || searchField.value === '' ) {
+			console.log(item.parentElement, "flex")
+			item.parentElement.style.display = "flex";
+		} else {
+			console.log(item.parentElement, "none");
+			item.parentElement.style.display = "none";
+		}
+	})
+})
+// View Show
+let views = document.querySelectorAll(".show i");
+views.forEach(view => {
+	view.addEventListener("click", e => {
+		views.forEach(view => {
+			view.classList.remove("on");
+		})
+		e.target.classList.add("on");
+	})
+})
+// Categories
+let categoriesItems = document.querySelectorAll(".categories li");
+categoriesItems.forEach(item => {
+	item.addEventListener("click", e => {
+		item.classList.toggle("on");
+		// console.log(item);
+	})
 })
